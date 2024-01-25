0a25d9eaf53b8da6a27774b8a2cac3d2b538450e
BennyKrup <krupkinbenyamin@gmail.com>
2024-01-24 23:52:39 -0800

    Added tests

--- test/test_bfs.py
+++ test/test_bfs.py
@@ -22,6 +22,13 @@ def test_bfs_traversal():
     expected_order = list(nx.bfs_tree(graph.graph, source=start_node))
     assert result == expected_order, "BFS traversal order is incorrect"
 
+    #compare the amount of nodes and edges with NetworkX's bfs order
+    expected_nodes = len(nx.bfs_tree(graph.graph, source=start_node).nodes())
+    expected_edges = len(nx.bfs_tree(graph.graph, source=start_node).edges())
+    assert len(result) == expected_nodes, "same amount of nodes"
+    assert len(result) -1 == expected_edges, "First node has no edge"
+
+
 
 def test_bfs():
     """
